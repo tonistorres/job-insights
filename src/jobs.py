d2a10f2d455ff8b4d65915ce5dd2efd78d464b89
tonistorres <sisvenda2011@gmail.com>
2022-08-04 17:35:44 -0300

    feat: implementing function read

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,20 @@
+import csv
 from functools import lru_cache
 
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
+    with open(path, encoding="utf-8") as file:
+        # Leitor e o escritor baseados em dicionários DictReader
+        # Vantagem - não precisamos manipular os índices para acessar os dados
+        # colunas
+        # Desvantagem - o espaço ocupado em memória (que é maior que o comum),
+        # devido à estrutura de dados utilizada.
+        jobs = csv.DictReader(file, delimiter=",", quotechar='"')
+        # Usando o conceito de desempacotamento
+        header, *data = jobs
+        return [header, *data]
+    pass
 
-    Parameters
-    ----------
-    path : str
-        Full path to file
 
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+
