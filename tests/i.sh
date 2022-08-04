6aef5487c05437342531abc5fe51a9ece23f0b76
tonistorres <sisvenda2011@gmail.com>
2022-08-04 18:40:55 -0300

    feat: add i.sh folder tests automate processes

---
+++ tests/i.sh
@@ -0,0 +1,45 @@
+MENU="
+---------------------------------
+Program Project Job Insights
+---------------------------------
+0 - Exit
+1 - Check error lint flake8
+2 - Execute function in file testing corresponding
+"
+
+OPTIONJOKER(){
+    echo -e "\033[31m -------------------------------------- \033[m"
+    echo -e "\033[31m Option $OPTION unknown \033[m"
+    echo -e "\033[31m -------------------------------------- \033[m"
+        echo -e "\033[31m [Exit 1s] \033[m"
+        sleep 1
+        exit 1     
+   echo -e "\033[31m -------------------------------------- \033[m"
+    
+}
+
+INPUT(){
+echo "$MENU "
+read -p "Enter the option [0,1] -->> " OPTION
+
+if [ "$OPTION" -eq 2 ];then
+read -p "Enter name function testing ex: test_xablau:" NAME
+python3 -m pytest -k $NAME
+exit 1
+fi
+
+}
+
+INPUT
+
+case "$OPTION" in
+        
+        1)
+        python3 -m flake8 
+        ;;
+               
+        *)
+        OPTIONJOKER
+        ;;
+esac      
+
