240ef28d88aca5401b98e5a5a254f67895be58e7
tonistorres <sisvenda2011@gmail.com>
2022-08-05 10:14:13 -0300

    fix: file temp temporary file for test

--- src/temp.py
+++ src/temp.py
@@ -4,33 +4,23 @@ from jobs import read
 path = "jobs.csv"
 
 
-def get_file_print_max_salries(path):
+def industries(path):
     dict = read(path)
-    arry_values = []
-    for dic in dict:
-        if dic["max_salary"] != "":
-            arry_values.append(int(dic["max_salary"]))
-    # https://www.tutorialspoint.com/python/list_max.htm
-    return max(arry_values)
+    industries = []
+    for job in dict:
+        if job["industry"] != '':
+            industries.append(job["industry"])
+    return [*set(industries)]
 
 
-result_max_value_list = get_file_print_max_salries(path)
-print(result_max_value_list)
+def industries_list_test(path):
+    dict = read(path)
+    results = set()
+    for element in dict:
+        if element["industry"] != '':
+            results.add(element["industry"])
+    return [*results]
 
-# def get_file_unic(path):
-#     dict = read(path)
-#     for dic in dict:
-#         print(dic)
 
-
-# get_file_unic(path)
-
-# result_unic = get_unique_job_types(path)
-# print("-------- Types unic:------")
-# print(result_unic)
-# print("--------------------------")
-
-# resultado = get_max_salary(path)
-# print("--- Max Value Salries: ---")
-# print(resultado)
-# print("--------------------------")
+arr_results = industries_list_test(path)
+print(arr_results)
